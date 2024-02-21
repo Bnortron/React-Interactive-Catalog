d54f4a845ceb0c82342d3bcc8a337327ca33864e
Brady Norton <bnort.11@gmail.com>
2024-02-21 10:42:31 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { createRoot } from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Title(props) {
+  return <h1>{props.title}</h1>;
 }
 
-export default App;
+class Catalog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filename: "images/img1.jpg",
+      alt: "image 1",
+      editing: false
+    };
+
+    this.editClick = this.editClick.bind(this);
+    this.saveClick = this.saveClick.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleAltChange = this.handleAltChange.bind(this);
+  }
+
+  editClick() {
+    this.setState({ editing: true });
+  }
+
+  saveClick() {
+    this.setState({ editing: false });
+  }
+
+  handleNameChange(event) {
+    this.setState({ filename: event.target.value });
+  }
+
+  handleAltChange(event) {
+    this.setState({ alt: event.target.value });
+  }
+
+  renderNormal() {
+    return (
+      <div>
+        <Title title="Lab06 - React Application" />
+        <h2>{this.state.alt}</h2>
+        <img src={this.state.filename} alt={this.state.alt} onClick={this.editClick} />
+      </div>
+    );
+  }
+
+  renderEdit() {
+    return (
+      <div>
+        <Title title="Lab06-React Application" />
+        <p>File Name:</p>
+        <select value={this.state.filename} onChange={this.handleNameChange}>
+          <option value="images/img1.jpg">Image 1</option>
+          <option value="images/img2.jpg">Image 2</option>
+          <option value="images/img3.jpg">Image 3</option>
+          <option value="images/img4.jpg">Image 4</option>
+        </select>
+        <br />
+        <p>Alt:</p>
+        <input type="text" value={this.state.alt} onChange={this.handleAltChange} />
+        <br />
+        <button onClick={this.saveClick}>Save</button>
+      </div>
+    );
+  }
+
+  render() {
+    if (this.state.editing) {
+      return this.renderEdit();
+    } else {
+      return this.renderNormal();
+    }
+  }
+}
+
+function Lab06App() {
+  return (
+    <Catalog />
+  )
+}
+
+const root = document.getElementById('react-lab');
+createRoot(root).render(<Lab06App />);
+
+export default Lab06App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,87 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Title(props) {
+  return <h1>{props.title}</h1>;
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Catalog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filename: "images/img1.jpg",
+      alt: "image 1",
+      editing: false
+    };
+
+    this.editClick = this.editClick.bind(this);
+    this.saveClick = this.saveClick.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleAltChange = this.handleAltChange.bind(this);
+  }
+
+  editClick() {
+    this.setState({ editing: true });
+  }
+
+  saveClick() {
+    this.setState({ editing: false });
+  }
+
+  handleNameChange(event) {
+    this.setState({ filename: event.target.value });
+  }
+
+  handleAltChange(event) {
+    this.setState({ alt: event.target.value });
+  }
+
+  renderNormal() {
+    return (
+      <div>
+        <Title title="Lab06 - React Application" />
+        <h2>{this.state.alt}</h2>
+        <img src={this.state.filename} alt={this.state.alt} onClick={this.editClick} />
+      </div>
+    );
+  }
+
+  renderEdit() {
+    return (
+      <div>
+        <Title title="Lab06-React Application" />
+        <p>File Name:</p>
+        <select value={this.state.filename} onChange={this.handleNameChange}>
+          <option value="images/img1.jpg">Image 1</option>
+          <option value="images/img2.jpg">Image 2</option>
+          <option value="images/img3.jpg">Image 3</option>
+          <option value="images/img4.jpg">Image 4</option>
+        </select>
+        <br />
+        <p>Alt:</p>
+        <input type="text" value={this.state.alt} onChange={this.handleAltChange} />
+        <br />
+        <button onClick={this.saveClick}>Save</button>
+      </div>
+    );
+  }
+
+  render() {
+    if (this.state.editing) {
+      return this.renderEdit();
+    } else {
+      return this.renderNormal();
+    }
+  }
+}
+
+function Lab06App() {
+  return (
+    <Catalog />
+  )
+}
+
+const root = document.getElementById('react-lab');
+createRoot(root).render(<Lab06App />);
+
+export default Lab06App;
